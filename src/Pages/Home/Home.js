7e9997bf54cb8fa76d5b224beed0cddd677daae8
diegobarata <porcocego177@gmail.com>
2021-11-09 10:32:24 -0300

    bug na home consertado e tela de quiz pronta

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,35 +1,34 @@
-import "./Home.css";
 import { Button, MenuItem, TextField } from "@material-ui/core";
-import Categories from "../../Data/Categories";
-import { useState, } from "react";
+import { useState } from "react";
 import { useHistory } from "react-router";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import Categories from "../../Data/Categories";
+import "./Home.css";
 
-const Home = ({name,setName, fetchQuestions}) => {
+const Home = ({ name, setName, fetchQuestions }) => {
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [error, setError] = useState(false);
 
-    const [category, setCategory] = useState("");
-    const [difficulty, setDifficulty] = useState("");
-    const [error, setError] = useState(false);
+  const history = useHistory();
 
-    const history = useHistory();
-
-    const handleSubmit = () => {
-        if(!category || !difficulty || !name){
-            setError(true);
-            return;
-        }
-        else{
-            setError(false);
-            fetchQuestions(category, difficulty);
-            history.push("/quiz");
-        }
+  const handleSubmit = () => {
+    if (!category || !difficulty || !name) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+      fetchQuestions(category, difficulty);
+      history.push("/quiz");
     }
+  };
+
   return (
     <div className="content">
       <div className="settings">
-        <span className="settings-text">Quiz Settings</span>
+        <span style={{ fontSize: 30 }}>Quiz Settings</span>
         <div className="settings__select">
-        {error && <ErrorMessage>Please Fill all the feilds</ErrorMessage>}
+          {error && <ErrorMessage>Please Fill all the feilds</ErrorMessage>}
           <TextField
             style={{ marginBottom: 25 }}
             label="Enter Your Name"
@@ -37,13 +36,12 @@ const Home = ({name,setName, fetchQuestions}) => {
             onChange={(e) => setName(e.target.value)}
           />
           <TextField
-            className="textField"
             select
             label="Select Category"
-            variant="outlined"
-            style={{ marginBottom: 25 }}
-            onChange={(e) => setCategory(e.target.value)}
             value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            variant="outlined"
+            style={{ marginBottom: 30 }}
           >
             {Categories.map((cat) => (
               <MenuItem key={cat.category} value={cat.value}>
@@ -52,33 +50,34 @@ const Home = ({name,setName, fetchQuestions}) => {
             ))}
           </TextField>
           <TextField
-            className="textField"
             select
             label="Select Difficulty"
-            variant="outlined"
-            style={{ marginBottom: 25 }}
-            onChange={(e) => setDifficulty(e.target.value)}
             value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+            variant="outlined"
+            style={{ marginBottom: 30 }}
           >
-              <MenuItem key="Easy" value="Easy">
-                  Easy
-               </MenuItem>
-               <MenuItem key="Medium" value="Medium">
-                  Medium
-               </MenuItem>
-               <MenuItem key="Hard" value="Hard">
-                  Hard
-               </MenuItem>
-
+            <MenuItem key="Easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+              Hard
+            </MenuItem>
           </TextField>
-          <Button onClick={handleSubmit} variant="contained" color="primary" size="large">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleSubmit}
+          >
             Start Quiz
           </Button>
-
         </div>
       </div>
-
-      <img src="/quizbanner.svg" className="banner" alt="quiz img" />
+      <img src="/quizbanner.svg" className="banner" alt="quiz app" />
     </div>
   );
 };
